600b4b7392409f8c1f14aaa5df9820843ad0a171
Felix Rieseberg <felix@felixrieseberg.com>
2015-10-06 15:38:14 -0700

    Setup Project Structure

---
+++ PowerTweetWeb/App/Source/main.js
@@ -0,0 +1,10 @@
+﻿(function () {
+    "use strict";
+
+    // The initialize function must be run each time a new page is loaded
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            app.initialize();
+        });
+    };
+})();
