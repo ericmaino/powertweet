32d875d587bc49e67650fd66d285747f4638bd7b
Janne Koskinen <jtjk@iki.fi>
2015-10-06 16:05:59 -0700

    Now generating hashtag

--- PowerTweetWeb/App/App.js
+++ PowerTweetWeb/App/App.js
@@ -5,6 +5,37 @@ var app = (function () {
 
     var app = {};
 
+    // The initialize function must be run each time a new page is loaded
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            app.initialize();
+            var tag = app.getHashTag();
+            $('.hashtag').html(tag);
+        });
+    };
+
+    Date.prototype.yyyymmdd = function () {
+        var yyyy = this.getFullYear().toString();
+        var mm = (this.getMonth() + 1).toString(); // getMonth() is zero-based
+        var dd = this.getDate().toString();
+        return yyyy + (mm[1] ? mm : "0" + mm[0]) + (dd[1] ? dd : "0" + dd[0]); // padding
+    };
+
+    function toTitleCase(str) {
+        return str.replace(/\w\S*/g, function (txt) { return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase(); });
+    }
+
+    app.getHashTag = function () {
+        var documentUrlSplitted = Office.context.document.url.split('\\');
+        var nameIndex = documentUrlSplitted.length - 1;
+        var name = documentUrlSplitted[nameIndex];
+        var prefixIndex = name.lastIndexOf(".");
+        if (prefixIndex > 0)
+            name = name.substring(0, prefixIndex);
+        var d = new Date();
+        return '#' + toTitleCase(name).replace(/ /g, '') + d.yyyymmdd();
+    };
+
     // Common initialization function (to be called from each page)
     app.initialize = function () {
         $('body').append(
