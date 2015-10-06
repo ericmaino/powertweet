4f326ffb748196dcbc58965f6f569dfe0368d120
Jason Poon <git@jasonpoon.ca>
2015-10-06 16:23:20 -0700

    add grunt. in powertweetweb project, you'll first need to do `npm install`, then `npm install -g grunt-cli` and then run `grunt`. you can also install a vs extension to run grunt

---
+++ PowerTweetWeb/Gruntfile.js
@@ -0,0 +1,31 @@
+﻿module.exports = function (grunt) {
+
+    grunt.loadNpmTasks("grunt-browserify");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+
+    grunt.initConfig({
+        browserify: {
+            dist: {
+                files: {
+                    "./App/Build/bundle.js": ["./App/**/*.es6.js"]
+                },
+                options: {
+                    transform: [
+                       ["babelify", {
+                           loose: "all"
+                       }]
+                    ]
+                }
+            }
+        },
+        watch: {
+            scripts: {
+                files: ["Gruntfile.js", "./App/**/*.es6.js"],
+                tasks: ["browserify"]
+            }
+        }
+    });
+
+    grunt.registerTask("default", ["build", "watch"]);
+    grunt.registerTask("build", ["browserify"]);
+};
