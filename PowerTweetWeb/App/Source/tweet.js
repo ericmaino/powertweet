3a825ba00bbeab77500f310d73cc4a3bae9119fd
Felix Rieseberg <felix@felixrieseberg.com>
2015-10-07 11:26:24 -0700

    Add ReactJS

---
+++ PowerTweetWeb/App/Source/tweet.js
@@ -0,0 +1,12 @@
+﻿var React = require('react'),
+    TweetComponent;
+
+TweetComponent = React.createClass({
+    render: function () {
+        return (
+            <span>{this.props.text}</span>
+        );
+    }
+});
+
+module.exports = TweetComponent;
