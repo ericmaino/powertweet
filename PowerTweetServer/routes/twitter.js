7b31e6c6a40381522096f299c7b337cec6fa21df
Jason Poon <git@jasonpoon.ca>
2015-10-07 11:11:30 -0700

    move configs to azure env vars

--- PowerTweetServer/routes/twitter.js
+++ PowerTweetServer/routes/twitter.js
@@ -2,20 +2,19 @@ var express = require('express');
 var router = express.Router();
 var Twit = require('twit')
 
+var T = new Twit({
+    consumer_key:         process.env.Twitter_Consumer_Key,
+  	consumer_secret:      process.env.Twitter_Consumer_Secret,
+   	access_token:         process.env.Twitter_Access_Token,
+    access_token_secret:  process.env.Twitter_Access_Token_Secret
+});
+
 /* GET users listing. */
 router.get('/:query', function(req, res, next) {
 	var consumer_key = req.query.consumerKey;
 	var consumer_secret = req.query.consumerSecret;
 	var access_token = req.query.accessToken;
 	var access_token_secret = req.query.accessTokenSecret;
-
-	var T = new Twit({
-		consumer_key:     		consumer_key,
-		consumer_secret:  		consumer_secret,    
-		access_token:       	access_token,
-		access_token_secret:  	access_token_secret,
-	});
-
 
 	T.get('search/tweets', { q: req.params.query, count: 100 }, function(err, data, response) {
 	  res.send(data);
